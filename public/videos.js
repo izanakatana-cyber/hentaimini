4d83cd58db1f9709849dcd590f7a74ed7bc10d73
furkanzz <furkanzz@example.com>
2026-09-17 00:03:35 +0300

    statik dosya yollari ve guvenlik duzeltmesi

--- public/videos.js
+++ public/videos.js
@@ -1,1 +1,14 @@
-window.STATIC_VIDEOS = [];
+window.STATIC_VIDEOS = [
+	{
+		id: 'stream-01',
+		title: 'Stream 01',
+		category: 'Genel',
+		type: 'embed',
+		embedSrc: 'https://voe.sx/e/st8vleemtqxb',
+		url: 'https://voe.sx/e/st8vleemtqxb',
+		embedCode: '<iframe src="https://voe.sx/e/st8vleemtqxb" width="560" height="315" scrolling="no" frameborder="0" allowfullscreen></iframe>',
+		duration: 'HD',
+		rating: '4.8',
+		createdAt: '2026-09-17T00:00:00.000Z'
+	}
+];
