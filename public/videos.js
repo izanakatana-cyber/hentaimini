e5a8dc44f4703b9cc0427e776b5f0a75feb56b1b
furkanzz <furkanzz@example.com>
2026-09-17 13:48:35 +0300

    statik dosya yollari ve guvenlik duzeltmesi

--- public/videos.js
+++ public/videos.js
@@ -1,5 +1,5 @@
-window.STATIC_VIDEOS = [  
-{
+window.STATIC_VIDEOS = [
+  {
     "id": "runtime-1789641075249",
     "title": "Yeni Video",
     "category": "Genel",
@@ -12,5 +12,33 @@ window.STATIC_VIDEOS = [
     "duration": "HD",
     "rating": "4.8",
     "createdAt": "2026-09-17T10:31:15.249Z"
+  },
+  {
+    "id": "runtime-1789641574685",
+    "title": "Yeni Video",
+    "category": "Genel",
+    "type": "embed",
+    "embedSrc": "https://voe.sx/e/abybpjik9usf",
+    "url": "https://voe.sx/e/abybpjik9usf",
+    "embedCode": "<iframe src=\"https://voe.sx/e/abybpjik9usf\" width=\"560\" height=\"315\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "thumbnail": "https://i.voe.sx/cache/abybpjik9usf_storyboard_L5.jpg",
+    "preview": "https://i.voe.sx/cache/abybpjik9usf_storyboard_L5.jpg",
+    "duration": "HD",
+    "rating": "4.8",
+    "createdAt": "2026-09-17T10:39:34.685Z"
+  },
+  {
+    "id": "runtime-1789641795387",
+    "title": "3",
+    "category": "Genel",
+    "type": "embed",
+    "embedSrc": "https://voe.sx/e/qiplemytabp3",
+    "url": "https://voe.sx/e/qiplemytabp3",
+    "embedCode": "<iframe src=\"https://voe.sx/e/qiplemytabp3\" width=\"560\" height=\"315\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "thumbnail": "https://i.voe.sx/cache/qiplemytabp3_storyboard_L5.jpg",
+    "preview": "https://i.voe.sx/cache/qiplemytabp3_storyboard_L5.jpg",
+    "duration": "HD",
+    "rating": "4.8",
+    "createdAt": "2026-09-17T10:43:15.387Z"
   }
 ];
