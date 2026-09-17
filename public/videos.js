e43524cbad5ac6d21eeeff25bf49801545abff9f
furkanzz <furkanzz@example.com>
2026-09-17 18:28:55 +0300

    statik dosya yollari ve guvenlik duzeltmesi

--- public/videos.js
+++ public/videos.js
@@ -1,5 +1,47 @@
 window.STATIC_VIDEOS = [
   {
+    "id": "runtime-1789658924243",
+    "title": "49",
+    "category": "Genel",
+    "type": "embed",
+    "embedSrc": "https://voe.sx/e/en2om8wmfno1",
+    "url": "https://voe.sx/en2om8wmfno1",
+    "embedCode": "<iframe src=\"https://voe.sx/e/en2om8wmfno1\" width=\"560\" height=\"315\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "thumbnail": "https://i.voe.sx/cache/en2om8wmfno1_storyboard_L5.jpg",
+    "preview": "https://i.voe.sx/cache/en2om8wmfno1_storyboard_L5.jpg",
+    "duration": "HD",
+    "rating": "4.8",
+    "createdAt": "2026-09-17T15:28:44.243294Z"
+  },
+  {
+    "id": "runtime-1789658862444",
+    "title": "48",
+    "category": "Genel",
+    "type": "embed",
+    "embedSrc": "https://voe.sx/e/qwhvpdywpqq8",
+    "url": "https://voe.sx/qwhvpdywpqq8",
+    "embedCode": "<iframe src=\"https://voe.sx/e/qwhvpdywpqq8\" width=\"560\" height=\"315\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "thumbnail": "https://i.voe.sx/cache/qwhvpdywpqq8_storyboard_L5.jpg",
+    "preview": "https://i.voe.sx/cache/qwhvpdywpqq8_storyboard_L5.jpg",
+    "duration": "HD",
+    "rating": "4.8",
+    "createdAt": "2026-09-17T15:27:42.444134Z"
+  },
+  {
+    "id": "runtime-1789658806900",
+    "title": "47",
+    "category": "Genel",
+    "type": "embed",
+    "embedSrc": "https://voe.sx/e/f5r6lwbmv1sj",
+    "url": "https://voe.sx/f5r6lwbmv1sj",
+    "embedCode": "<iframe src=\"https://voe.sx/e/f5r6lwbmv1sj\" width=\"560\" height=\"315\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "thumbnail": "https://i.voe.sx/cache/f5r6lwbmv1sj_storyboard_L5.jpg",
+    "preview": "https://i.voe.sx/cache/f5r6lwbmv1sj_storyboard_L5.jpg",
+    "duration": "HD",
+    "rating": "4.8",
+    "createdAt": "2026-09-17T15:26:46.900585Z"
+  },
+  {
     "id": "runtime-1789658306859",
     "title": "45",
     "category": "Genel",
